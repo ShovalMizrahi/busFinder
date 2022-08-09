1b57664d716612742548bc0091688fba373bdf9b
ShovalMizrahi <shovalmizrahi62@gmail.com>
2022-08-09 19:34:31 +0300

    update

--- app/src/main/java/com/example/busfinder/Line.java
+++ app/src/main/java/com/example/busfinder/Line.java
@@ -4,6 +4,7 @@ public class Line {
     private String id;
     private String number;
     private String companyID;
+    public static ArrayListStation line_stations;
 
 
     public Line(String id, String number, String companyID) {
@@ -34,5 +35,26 @@ public class Line {
 
     public void setCompanyID(String companyID) {
         this.companyID = companyID;
+    }
+
+    public static void addStationsOfLine(){
+        for (int i = 0; i < RestApi.lines.size(); i++) {
+
+            RestApi.lines.get(i).line_stations = new ArrayListStation();
+
+
+            for (int j = 0; j < RestApi.tracks.size(); j++) {
+
+                if (RestApi.tracks.get(j).getLineID().equals(RestApi.lines.get(i).getId())) {
+                    ArrayListStation arrayListStation = new ArrayListStation();
+                    Station station = new Station(arrayListStation.findStationById(RestApi.tracks.get(j).getStationID()));
+
+                    RestApi.lines.get(i).line_stations.add(station);
+
+                }
+
+            }
+
+        }
     }
 }
